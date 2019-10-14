2b2aaa3e217c54444918e1dee3b9c3a6fef21246
Viktor Ferenczi <ferencziviktor11@gmail.com>
2019-10-14 11:16:08 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -61,7 +61,15 @@ def print_menu(title, list_options, exit_message):
         None: This function doesn't return anything it only prints to console.
     """
 
-    # your code
+    
+    
+    
+    
+    
+    
+    print("Main menu:\n\t(1) Store manager\n\t(2) Human resources manager\n\t(3) "
+          "Inventory manager\n\t(4) Accounting manager"
+         "\n\t(5) Sales manager\n\t(6) Customer relationship management (CRM)\n\t(0) Back to main menu")
 
 
 def get_inputs(list_labels, title):
@@ -83,11 +91,31 @@ def get_inputs(list_labels, title):
         list: List of data given by the user. Sample return:
             [<user_input_1>, <user_input_2>, <user_input_3>]
     """
+    
+
+    
+    
+    
+    
+    
+    
     inputs = []
 
-    # your code
+    print("Please provide your personal information:")
+    while True:
+        try:
+            firstname = input("First name: ")
+            lastname = input("Last name: ")
+            age = int(input("Your age: "))
+            inputs.append(firstname)
+            inputs.append(lastname)
+            inputs.append(str(age))
+            return inputs
+        except ValueError:
+            print("Ops! Something went wrong.\nError: Wrong input in the following: Your age\nPlease try again.")
+            continue
 
-    return inputs
+  
 
 
 def print_error_message(message):
@@ -101,4 +129,7 @@ def print_error_message(message):
         None: This function doesn't return anything it only prints to console.
     """
 
-    # your code
+    
+    
+    
+    print("Ops! An error occurred!\n Error: "+message)
