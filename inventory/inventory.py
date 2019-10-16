7bfbb52d6982b864fc3625e1e4c0a3d6033123f7
gergodanko <55234339+gergodanko@users.noreply.github.com>
2019-10-16 10:40:01 +0200

    Add files via upload

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -29,6 +29,30 @@ def start_module():
     """
 
     # your code
+    options=["show table","add", "remove","Update","Get available items","Get average durability times"]
+    table = data_manager.get_table_from_file("inventory/inventory.csv")
+    while True:
+        ui.print_menu("Inventory",options,"Main menu")
+        inputs=ui.get_inputs("Please enter a number:","")
+        option = inputs[0]
+        if option=="1":
+            show_table(table)
+        elif option=="2":
+            table = add(table)
+            #data_manager.write_table_to_file(table, "hr/persons.csv")
+        elif option=="3":
+            userinput = ui.get_inputs("Enter the ID you want to remove: ", "")
+            table =remove(table,userinput)
+        elif option=="4":
+            userinput_id = ui.get_inputs("Enter the ID you want to update: ", "")
+            update(table,userinput_id)
+        elif option=="5":
+            userinput_id = ui.get_inputs("Enter the year: ", "")
+            ui.print_result(get_available_items(table, userinput_id),"")
+        elif option=="6":
+            ui.print_result(get_average_durability_by_manufacturers(table),"")
+        elif option=="0":
+            break
 
 
 def show_table(table):
@@ -43,6 +67,9 @@ def show_table(table):
     """
 
     # your code
+    headers=["id","name","manufacturer","purchase year", "durability"]
+    ui.print_table(table,headers)
+    ui.get_inputs("Press a button to advance!","")
 
 
 def add(table):
@@ -57,8 +84,16 @@ def add(table):
     """
 
     # your code
-
+    table.append([])
+    table[-1].append(common.generate_random(table))
+    table[-1].append(ui.get_inputs("Type in the name: ",""))
+    table[-1].append(ui.get_inputs("Type in the manufacturer: ",""))
+    table[-1].append(ui.get_inputs("Type in the purchase year: ",""))
+    table[-1].append(ui.get_inputs("Type in the durability",""))
+    
     return table
+
+    
 
 
 def remove(table, id_):
@@ -74,7 +109,10 @@ def remove(table, id_):
     """
 
     # your code
-
+    for sublist in table:
+        if id_ in sublist:
+            table.remove(sublist)
+    ui.get_inputs("Press Enter to to advance!", "")
     return table
 
 
@@ -91,6 +129,19 @@ def update(table, id_):
     """
 
     # your code
+    userinput_name = ui.get_inputs("Type in the name: ","")
+    userinput_manufacturer = ui.get_inputs("Type in the manufacturer:  ", "")
+    userinput_purchase = ui.get_inputs("Type in the purchase year:  ", "")
+    userinput_durability = ui.get_inputs("Type in the durability","")
+
+    
+    for sublist in table:
+        if id_ in sublist:
+            sublist[0] = common.generate_random(table)
+            sublist[1] = userinput_name
+            sublist[2] = userinput_manufacturer
+            sublist[3] = userinput_purchase
+            sublist[4] = userinput_durability
 
     return table
 
@@ -111,6 +162,15 @@ def get_available_items(table, year):
     """
 
     # your code
+    available_items=[]
+    DURABILITY_COL=4
+    YEAR_COL=3
+    ITEM_COL=1
+    for i in range(len(table)):
+        if int(table[i][DURABILITY_COL])+int(table[i][YEAR_COL])>=int(year) and int(year)>=int(table[i][YEAR_COL]):
+            available_items.append(table[i])
+
+    return available_items
 
 
 def get_average_durability_by_manufacturers(table):
@@ -125,3 +185,22 @@ def get_average_durability_by_manufacturers(table):
     """
 
     # your code
+    avg_durability={}
+    DURABILITY_COL=4
+    MANUFACTURER_COL=2
+    temp=0
+    
+    for i in range(len(table)):
+        if table[i][MANUFACTURER_COL]not in avg_durability.keys():
+            sum_dur=0
+            counter=0
+            avg=0
+            for j in range(len(table)):
+                if table[i][MANUFACTURER_COL]==table[j][MANUFACTURER_COL]:
+                    sum_dur+=int(table[j][DURABILITY_COL])
+                    counter+=1
+            avg=sum_dur/counter
+            avg_durability.update({table[i][MANUFACTURER_COL]:avg})
+    return avg_durability
+
+
