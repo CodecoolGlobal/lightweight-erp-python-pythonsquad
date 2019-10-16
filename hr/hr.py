548ec62171a25daf6d6c87c0df3694906ce9665e
gergodanko <55234339+gergodanko@users.noreply.github.com>
2019-10-16 10:39:30 +0200

    Add files via upload

--- hr/hr.py
+++ hr/hr.py
@@ -36,7 +36,16 @@ def start_module():
         elif option=="2":
             table = add(table)
             #data_manager.write_table_to_file(table, "hr/persons.csv")
-        
+        elif option=="3":
+            userinput = ui.get_inputs("Enter the ID you want to remove: ", "")
+            table =remove(table,userinput)
+        elif option=="4":
+            userinput_id = ui.get_inputs("Enter the ID you want to update: ", "")
+            update(table,userinput_id)
+        elif option=="5":
+            get_oldest_person(table)
+        elif option=="6":
+            get_persons_closest_to_average(table)
         elif option=="0":
             break
     
@@ -94,7 +103,10 @@ def remove(table, id_):
     """
 
     # your code
-    
+    for sublist in table:
+        if id_ in sublist:
+            table.remove(sublist)
+    ui.get_inputs("Press Enter to to advance!", "")
     return table
 
 
@@ -111,7 +123,15 @@ def update(table, id_):
     """
 
     # your code
-
+    userinput_name = ui.get_inputs("Name: ","")
+    userinput_year = ui.get_inputs("Birth year: ", "")
+    
+    for sublist in table:
+        if id_ in sublist:
+            sublist[0] = common.generate_random(table)
+            sublist[1] = userinput_name
+            sublist[2] = userinput_year
+        
     return table
 
 
@@ -130,6 +150,18 @@ def get_oldest_person(table):
     """
 
     # your code
+    oldest_list=[]
+    oldest_birth_year=int(table[0][2])
+    for i in range(len(table)):
+        if int(table[i][2])<oldest_birth_year:
+            oldest_birth_year=int(table[i][2])
+    for j in range(len(table)):
+        if int(table[j][2])==oldest_birth_year:
+            oldest_list.append(table[j][1])
+    ui.print_result(",".join(oldest_list),"")
+    ui.get_inputs("Press Enter to to advance!", "")
+    return oldest_list
+    
 
 
 def get_persons_closest_to_average(table):
@@ -144,4 +176,24 @@ def get_persons_closest_to_average(table):
     """
 
     # your code
+    ages=[]
+    avg=0
+    list_of_names=[]
+    for i in range(len(table)):
+        ages.append(2019-int(table[i][2]))
+    closest_to_avg=abs(ages[0]-avg)
+    avg=common.avg_of_list(ages)
+    for j in range(len(ages)):
+        if abs(ages[j]-avg)<closest_to_avg:
+            closest_to_avg=abs(ages[j]-avg)
+    for k in range(len(table)):
+        if abs((2019-int(table[k][2]))-avg)==closest_to_avg:
+            list_of_names.append(table[k][1])
+    ui.print_result(list_of_names,"")
+    ui.get_inputs("Press Enter to to advance!", "")        
+    return list_of_names
+
+
+
+        
 
