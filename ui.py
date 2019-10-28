e235568fb2b2ca4bca91d8597135e5106e98fc5e
gergodanko <55234339+gergodanko@users.noreply.github.com>
2019-10-28 09:39:23 +0100

    Add files via upload

--- ui.py
+++ ui.py
@@ -23,30 +23,45 @@ def print_table(table, title_list):
     """
 
     # your goes code
+    sizes=[]
+    sum_of_sizes=0
     temp=1
+    for i in range(len(table[0])):
+        n=0
+        for j in range(len(table)):
+            if len(table[j][i])>n:
+                n=len(table[j][i])
+        if len(title_list[i])>n:
+            n=len(title_list[i])
+        sizes.append(n)
+    size_pointer=0
+    for size in sizes:
+        sum_of_sizes+=size
+    print("-"*(sum_of_sizes+(len(table[0]))+1))
     for word in title_list:
         if temp==len(title_list):
-            print("|{0:^20}".format(word), end="|\n")
+            print("|{0:^{1}}".format(word,sizes[size_pointer]), end="|\n")
         else:
-            print("|{0:^20}".format(word), end="")
+            print("|{0:^{1}}".format(word,sizes[size_pointer]), end="")
         temp+=1
-    print("-"*64)
+        size_pointer+=1
+    print("-"*(sum_of_sizes+(len(table[0]))+1))
+    
+    
     for row in table:
+        size_pointer=0
         temp=1
         for item in row:
             if temp==len(row):
-                print("|{0:^20}".format(item), end="|\n")
+                print("|{0:^{1}}".format(item,sizes[size_pointer]), end="|\n")
             else:
-                print("|{0:^20}".format(item) ,end="")
-            
-            
-
+                print("|{0:^{1}}".format(item,sizes[size_pointer]) ,end="")
+            size_pointer+=1
             temp+=1
-
-    
-
-
-
+        if temp==len(table[0]):
+            print("-"*(sum_of_sizes+(len(table[0]))+1))
+        else:
+            print("|"+"-"*(sum_of_sizes+(len(table[0]))-1)+"|")
 
 def print_result(result, label):
     """
